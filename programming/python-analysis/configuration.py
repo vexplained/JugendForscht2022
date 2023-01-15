01895389ad53482fc7ced877996ed4c3ee347f9c
vexplained <vexplained@users.noreply.github.com>
2023-01-15 23:01:38 +0100

    Arbeit fertig

--- programming/python-analysis/configuration.py
+++ programming/python-analysis/configuration.py
@@ -5,8 +5,8 @@ input_filename: str = "2"
 averaging_resolution: int = 12
 
 curve_fitter = dict(
-    weighting_algorithm = "square", # exp, square, cube
-    sign_removal = "abs", # abs, squaring
-    precision_pow10 = -5,
-    max_pow10 = 3,
+    weighting_algorithm="square",  # exp, square, cube
+    sign_removal="abs",  # abs, squaring
+    precision_pow10=-5,
+    max_pow10=1,
 )
